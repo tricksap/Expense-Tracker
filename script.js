92cb9bc49abb18fa213d36acb7867cf8e384342d
Patrick <tricksap@gmail.com>
2021-04-05 08:41:40 +0800

    add table and total balance

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Income() {
     name: $("#name").val(),
     amount: $("#amount").val(),
     date: $("#date").val(),
+    type: "income",
   };
   transactions.push(transaction);
   $("#total-income").text(
@@ -22,6 +23,8 @@ function Income() {
   $("#name").val(" ");
   $("#amount").val(" ");
   $("#date").val(" ");
+  Balance();
+  Table();
 }
 
 function Expense() {
@@ -29,6 +32,7 @@ function Expense() {
     name: $("#name").val(),
     amount: "-" + $("#amount").val(),
     date: $("#date").val(),
+    type: "expense",
   };
   transactions.push(transaction);
   $("#total-expense").text(
@@ -38,4 +42,31 @@ function Expense() {
   $("#name").val(" ");
   $("#amount").val(" ");
   $("#date").val(" ");
+  Balance();
+  Table();
+}
+function Balance() {
+  $("#balance").text(
+    parseInt($("#total-income").text()) + parseInt($("#total-expense").text())
+  );
+}
+
+function Table() {
+  $("tbody").text("");
+  transactions.forEach(function (element) {
+    $("tbody").prepend(
+      "<tr><td>" +
+        element.name +
+        "</td> <td>" +
+        element.amount +
+        "</td> <td>" +
+        element.date +
+        "</td></tr>"
+    );
+    if (element.type === "income") {
+      $("tbody tr").addClass("table-success");
+    } else if (element.type === "expense") {
+      $("tbody tr").addClass("table-danger");
+    }
+  });
 }
